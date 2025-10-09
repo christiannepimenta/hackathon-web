7c974d6573642c5eefe378044b3cb2608df2724b
christiannepimenta <christiannepimenta@gmail.com>
2025-10-09 17:04:22 -0300

    Update page.js

--- app/forbidden/page.js
+++ app/forbidden/page.js
@@ -3,7 +3,7 @@ export default function Forbidden() {
   return (
     <main className="grid">
       <section className="card">
-        <h2 style={{marginTop:0}}>Acesso negado</h2>
+        <h2 style={{ marginTop: 0 }}>Acesso negado</h2>
         <p className="note">Você não tem permissão para acessar esta página.</p>
         <p><a href="/">Voltar para a home</a></p>
       </section>
